dbe2db2936d79304edf03d9566eab05506e44e89
Matheus Afonso de Souza <42722634+Matheusafonsouza@users.noreply.github.com>
2024-12-07 14:54:12 -0300

    feat: admin users table (#2)

---
+++ src/database/entities/admin-user.entity.ts
@@ -0,0 +1,31 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class AdminUser {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  email: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
