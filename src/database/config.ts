2028d291b5a249db5fbff3ffd9e7ffe769704958
Matheusafonsouza <matheusafonsouza@gmail.com>
2024-12-07 11:42:01 -0300

    chore: add created at and updated at columns

--- src/database/config.ts
+++ src/database/config.ts
@@ -1,23 +1,23 @@
-import { registerAs } from "@nestjs/config";
+import { registerAs } from '@nestjs/config';
 import { config as dotenvConfig } from 'dotenv';
-import { join } from "path";
-import { DataSource, DataSourceOptions } from "typeorm";
+import { join } from 'path';
+import { DataSource, DataSourceOptions } from 'typeorm';
 
 dotenvConfig({ path: '.env' });
 
 const config = {
-    type: 'postgres',
-    host: `${process.env.DB_HOST}`,
-    port: `${process.env.DB_PORT}`,
-    username: `${process.env.DB_USER}`,
-    password: `${process.env.DB_PASSWORD}`,
-    database: `${process.env.DB_NAME}`,
-    entities: [join(__dirname, '**', '*.entity.{ts,js}')],
-    migrations: [join(__dirname, 'migrations', '*.{ts,js}')],
-    autoLoadEntities: true,
-    synchronize: false,
-    ssl: { rejectUnauthorized: false },
-}
+  type: 'postgres',
+  host: `${process.env.DB_HOST}`,
+  port: `${process.env.DB_PORT}`,
+  username: `${process.env.DB_USER}`,
+  password: `${process.env.DB_PASSWORD}`,
+  database: `${process.env.DB_NAME}`,
+  entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+  migrations: [join(__dirname, 'migrations', '*.{ts,js}')],
+  autoLoadEntities: true,
+  synchronize: false,
+  ssl: { rejectUnauthorized: false },
+};
 
-export default registerAs('typeorm', () => config)
+export default registerAs('typeorm', () => config);
 export const connectionSource = new DataSource(config as DataSourceOptions);
--- src/database/migrations/1733537718312-users-table.ts
+++ src/database/migrations/1733537718312-users-table.ts
@@ -1,14 +1,15 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class UsersTable1733537718312 implements MigrationInterface {
-    name = 'UsersTable1733537718312'
+  name = 'UsersTable1733537718312';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "user" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "firstName" character varying NOT NULL, "lastName" character varying NOT NULL, "password" character varying NOT NULL, "email" character varying NOT NULL, CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "user" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "firstName" character varying NOT NULL, "lastName" character varying NOT NULL, "password" character varying NOT NULL, "email" character varying NOT NULL, CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE "user"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "user"`);
+  }
 }
